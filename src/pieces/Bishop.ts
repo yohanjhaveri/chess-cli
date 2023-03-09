dd207057da818ce3fe28f1713d2d39168d23990a
mdgoldstein2 <mic.dan.goldstein@gmail.com>
2023-03-08 21:40:05 -0500

    defines piece classes

---
+++ src/pieces/Bishop.ts
@@ -0,0 +1,35 @@
+import { PIECES } from '../constants';
+import { Piece } from "./Piece";
+import { Coordinate, PieceColor, PieceDisplay } from "../types";
+
+export class Bishop extends Piece {
+  constructor(color: PieceColor) {
+    const display = {
+      W: PIECES.WB,
+      B: PIECES.BB
+    }[color] as PieceDisplay
+
+    super(color, 3, display);
+  }
+
+  getAllMoves(position: Coordinate): Coordinate[] {
+    return this.generatePositions(position);
+  }
+
+  getAllAttacks(position: Coordinate): Coordinate[] {
+    return this.generatePositions(position);
+  }
+
+  private generatePositions([x, y]: Coordinate): Coordinate[] {
+    const moves: Coordinate[] = [];
+
+    for (let index = 1; index < 8; index++) {
+      moves.push([x - index, y - index]); // up left
+      moves.push([x + index, y - index]); // down left
+      moves.push([x - index, y + index]); // up right
+      moves.push([x + index, y + index]); // down right
+    }
+
+    return moves;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,16 @@
 export type Coordinate = [number, number];
 export type PieceColor = "W" | "B";
-export type PiecePoints = 1 | 3 | 5 | 9;
-export type PieceDisplay = "♜" | "♖" | "♞" | "♘" | "♝" | "♗" | "♛" | "♕" | "♚" | "♔" | "♟︎" | "♙" 
+export type PiecePoints = -1 | 1 | 3 | 5 | 9;
+export type PieceDisplay =
+  | "♜"
+  | "♖"
+  | "♞"
+  | "♘"
+  | "♝"
+  | "♗"
+  | "♛"
+  | "♕"
+  | "♚"
+  | "♔"
+  | "♟︎"
+  | "♙";
