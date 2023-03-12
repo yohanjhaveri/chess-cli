75aebbfdd4f446ea932c00398a858c3b400d3b82
mdgoldstein2 <mic.dan.goldstein@gmail.com>
2023-03-12 16:17:18 -0400

    add converter to interpreter function

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,16 +1,30 @@
 import { PIECE_NAMES } from "./constants";
+import { Coordinate, PieceName } from "./types";
 
-export const getMove = (inputString: String) => {
+type MoveResult = {
+  piece: PieceName | null;
+  destination: Coordinate | null;
+};
+
+const letterOffset = 97; // ascii offset for letter a
+const numberOffset = 49; // ascii offset for the number 1 as a character
+
+export const getMove = (inputString: String): MoveResult => {
   const inputWords = inputString
     .toLowerCase()
     .replace(/[^a-zA-Z0-9 ]/g, "")
     .split(" ");
 
-  const piece = inputWords.find((word) => PIECE_NAMES.has(word));
+  const piece = inputWords.find((word) => word in PIECE_NAMES);
   const destination = inputWords.find((word) => /^[a-h][1-8]$/.test(word));
 
   return {
-    piece: piece || null,
-    destination: destination || null,
+    piece: piece ? PIECE_NAMES[piece] : null,
+    destination: destination
+      ? [
+          destination.charCodeAt(0) - letterOffset,
+          destination.charCodeAt(1) - numberOffset,
+        ]
+      : null,
   };
 };
