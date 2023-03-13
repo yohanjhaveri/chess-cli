6f1741d0110768b1f145a1cb3f9a9d2dc2995714
mdgoldstein2 <mic.dan.goldstein@gmail.com>
2023-03-13 18:11:50 -0400

    add castle to interpreter

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,19 +1,33 @@
 import { PIECE_NAMES } from "./constants";
-import { Coordinate, PieceName } from "./types";
+import { Coordinate, PieceName, Castle } from "./types";
 
 type MoveResult = {
   piece: PieceName | null;
-  destination: Coordinate | null;
+  destination: Coordinate | Castle | null;
 };
 
-const letterOffset = 97; // ascii offset for letter a
-const numberOffset = 49; // ascii offset for the number 1 as a character
+const LETTER_OFFSET = 97; // ascii offset for letter a
+const NUMBER_OFFSET = 49; // ascii offset for the number 1 as a character
 
 export const getMove = (inputString: String): MoveResult => {
   const inputWords = inputString
     .toLowerCase()
     .replace(/[^a-zA-Z0-9 ]/g, "")
     .split(" ");
+
+  if (inputWords.includes("castle") || inputWords.includes("castles")) {
+    if (inputWords.includes("queen")) {
+      return {
+        piece: "K",
+        destination: "QC",
+      };
+    } else {
+      return {
+        piece: "K",
+        destination: "KC",
+      };
+    }
+  }
 
   const piece = inputWords.find((word) => word in PIECE_NAMES);
   const destination = inputWords.find((word) => /^[a-h][1-8]$/.test(word));
@@ -22,8 +36,8 @@ export const getMove = (inputString: String): MoveResult => {
     piece: piece ? PIECE_NAMES[piece] : null,
     destination: destination
       ? [
-          destination.charCodeAt(0) - letterOffset,
-          destination.charCodeAt(1) - numberOffset,
+          destination.charCodeAt(0) - LETTER_OFFSET,
+          destination.charCodeAt(1) - NUMBER_OFFSET,
         ]
       : null,
   };
--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,4 @@ export type PieceName = "R" | "N" | "B" | "Q" | "K" | "P";
 export type PieceColor = "W" | "B";
 export type PiecePoints = -1 | 1 | 3 | 5 | 9;
 export type PieceDisplay = "♜" | "♞" | "♝" | "♛" | "♚" | "♟︎";
+export type Castle = "QC" | "KC";
